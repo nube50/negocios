6ce43e6d3e4b36e7dbde579630647b28d23588a6
nube50 <dajomondra@gmail.com>
2026-06-24 15:59:30 -0500

    feat: add PWA support (manifest, SW, icon) - app instalable 'Gestión Negocios'

---
+++ busqueda/sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'gestion-negocios-v1';
+const FILES = [
+  '.',
+  'index.html',
+  'manifest.json',
+  'icon.svg',
+  'datos.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => { if (k !== CACHE) return caches.delete(k); })))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
